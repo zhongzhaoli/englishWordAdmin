0a54f292f12ad29fa302d4549c4a1c9950ba5cb4
zhongzhaoli <525966315@qq.com>
2023-06-05 21:45:38 +0800

    env

--- src/api/request.js
+++ src/api/request.js
@@ -1,14 +1,14 @@
 /*
  * @Author: Custer
  * @Date: 2021-09-08 09:55:36
- * @LastEditors: Custer
- * @LastEditTime: 2021-10-16 02:07:33
+ * @LastEditors: zhongzhaoli 525966315@qq.com
+ * @LastEditTime: 2023-06-05 21:12:38
  * @Description: file content
  */
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: '/api'
+  baseURL: process.env.VUE_APP_API + '/api'
 })
 
 export default service;
